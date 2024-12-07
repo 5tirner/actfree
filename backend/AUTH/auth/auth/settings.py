b73d2c015db371a6162d5d555dde2ae1a1ea0003
5tirner <thatfor1337@gmail.com>
2024-12-07 14:51:39 +0100

    Add auth

--- backend/AUTH/auth/auth/settings.py
+++ backend/AUTH/auth/auth/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework_simplejwt',
+    # 'rest_framework_simplejwt',
     'rest_framework',
     'authapp',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
